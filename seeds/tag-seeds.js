6e1a66f1d49cc0235cc4405220d7839eab8c0dc9
DiogoS77 <diogo.messi.18@hotmail.com>
2023-07-11 20:49:11 +0100

    added the tags according to the products

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -2,28 +2,13 @@ const { Tag } = require('../models');
 
 const tagData = [
   {
-    tag_name: 'rock music',
+    tag_name: 'grey',
   },
   {
-    tag_name: 'pop music',
-  },
-  {
-    tag_name: 'blue',
-  },
-  {
-    tag_name: 'red',
-  },
-  {
-    tag_name: 'green',
+    tag_name: 'black',
   },
   {
     tag_name: 'white',
-  },
-  {
-    tag_name: 'gold',
-  },
-  {
-    tag_name: 'pop culture',
   },
 ];
 
