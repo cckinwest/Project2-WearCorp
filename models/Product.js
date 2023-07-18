f4ac5cdf3449c70a6e7ac8ba8ac54d38b0a317e9
DiogoS77 <diogo.messi.18@hotmail.com>
2023-07-18 20:32:08 +0100

    added images to product page, adjusted css

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
-const sequelize = require("../config/connection");
+const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -27,6 +27,11 @@ Product.init(
         isDecimal: true,
       },
     },
+    imgurl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'placeholder.jpg',
+    },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -38,8 +43,8 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "category",
-        key: "id",
+        model: 'category',
+        key: 'id',
       },
     },
   },
@@ -48,7 +53,7 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "product",
+    modelName: 'product',
   }
 );
 
